aa8d4d771f7c0c5a1d1f60187b747128f0dd843a
matthewhuntt <Matthew@lawn-128-61-44-137.lawn.gatech.edu>
2019-02-12 14:40:12 -0500

    Reader and echelonDict functional

--- Transform.py
+++ Transform.py
@@ -1,1 +1,25 @@
 
+import csv
+
+def csvReader(filename):
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        rows = []
+        for row in reader:
+            rows.append(row)
+    echelonDict = {}
+    for row in rows:
+        if row[1] not in echelonDict.values():
+            echelonDict[len(echelonDict) + 1] = row[1]
+    return echelonDict
+
+def constructor():
+    return None
+
+def main(args):
+    print(csvReader("SetupData.csv"))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
+
