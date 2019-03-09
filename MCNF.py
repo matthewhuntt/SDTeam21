cc61e708565aed056fb98820ec60afe94e7856ac
Ronnie <rsfalzone@gmail.com>
2019-03-09 12:40:46 -0500

    documented, added 'TODOs', refactored Lagrange & Objective

--- MCNF.py
+++ MCNF.py
@@ -11,6 +11,7 @@ def arcDataReader(filename):
 
     # Formats data into Network Components
     arcDict = {}    # (tail, head, commodity): (lb, ub, cost)
+    lagrangeDict = {} # node: multiplier
     nodeList = []
     commodityList = []
     for row in rows[1:]: # Removes Header
@@ -22,63 +23,100 @@ def arcDataReader(filename):
             ub= row[8]      # Upper Bound of Arc
             cost = row[9]   # Cost/unit of Arc
 
+            # Update nodeList: (Room_ID, Time_Echelon, Dummy)
             for node in [tail, head]:
                 if node not in nodeList:
                     nodeList.append(node)
+
+                    # TODO: Cleaner way, & why set to 1?
+                    if node[0][0] == "S":
+                        lagrangeDict[node] = 1
+
+            # Update commodityList
             if commodity not in commodityList:
                 commodityList.append(commodity)
 
+            # Updates arcDict
             arcDict[(tail, head, commodity)] = (lb, ub, cost)
 
-    return arcDict, nodeList, commodityList
+    return arcDict, lagrangeDict, nodeList, commodityList
 
-def roomDictReader(filename):
+
+# TODO: Mix with Langrange Multiplier Dict/ Node list?
+def roomKeyReader(filename):
     with open (filename, "r") as f:
         reader = csv.reader(f)
         rows = []
         for row in reader:
             rows.append(row)
-    roomDict = {}
+
+    roomKey = {}
+    # eventRooms = {}
+    # storageRooms = {}
     for row in rows[1:]:
         if len(row) > 0:
-            roomDict[row[0]] = row[1]
-    return roomDict
+            roomKey[row[0]] = row[1]
+    return roomKey
 
-def modeler(arcDict, nodeList, commodityList, roomDict, roomCapDict, commodityVolDict):
+
+# Pass in MCNF Object instead of all the individual dicts.
+# Cleaner? See OO Principle: Encapsulation
+def modeler(arcDict, lagrangeDict, nodeList, commodityList, roomKey, roomCapDict, commodityVolDict):
     m = Model("m")
     varDict = {}
     for arc in arcDict:
         lowerBound = float(arcDict[arc][0])
         upperBound = float(arcDict[arc][1])
-        cost = float(arcDict[arc][2])
+        cost = float(arcDict[arc][2]) # Useles rn
+        # TODO:
+        # If we can figure out how to access the cost from
+        # the variable, then it can be useful. Otherwise its
+        # a waste.
+        # It then follows that, since the costs come from
+        # the Dijkstra Matrix, its redundant to have it in
+        # the arcDict.
+        # Is there a more concise way to create arcs?
+
+        # TODO: Cleaner Way? Toss to a helper function to create var and document?
         name = "(({}, {}, {}), ({}, {}, {}), {})".format(arc[0][0], arc[0][1], arc[0][2], arc[1][0], arc[1][1], arc[1][2], arc[2])
         varDict[arc] = m.addVar(lb=lowerBound, ub=upperBound, obj=cost, name=name)
 
-    lagrangeDict = {}
-    for node in nodeList:
-        if node[0][0] == "S":
-            lagrangeDict[node] = 1
+        # TODO:
+        # Puropose of both Var and Arc Dicts?
+        # We run through one two create the other, so why
+        # not create varDict from the start?
+        # If its just for ease of accesing data, we should
+        # figure out how to do it with just varDict.
+
+### LAGRANGE & OBJ
+# MOVED TO arcDictReader
+    # lagrangeDict = {}
+    # for node in nodeList:
+    #     if node[0][0] == "S":
+    #         lagrangeDict[node] = 1
     # print(lagrangeDict)
 
-#
-# Refactor to a method. obj will be rewritten for each itteration of subgradient
-#
-    objective = LinExpr()
+    # Two parts: mcnfObj and penatly term.
+    # to rewrite, add constant to output from penalty_term()
+    # rewrite in quick helper funct. ***
+    objective = LinExpr() #***
+
+    # KEEP THIS as a constant for easy rewriting
+    mcnfObj = LinExpr()
     for arc in arcDict.keys():
-        # print(varDict[arc], arcDict[arc][2])
-        objective.add(varDict[arc], arcDict[arc][2])
+        mcnfObj.add(varDict[arc], arcDict[arc][2])
 
     # for i in range(objective.size()):
     #     print(objective.getVar(i), objective.getCoeff(i))
 
     ## create a mapping of node to vars to make (Ax-b) easier to calculate
     penalty = penalty_term(nodeList, commodityList, arcDict, varDict, commodityVolDict, lagrangeDict)
-    objective.add(penalty)
-#
-# End of method
-#
 
+    objective.add(mcnfObj) #***
+    objective.add(penalty) #***
+### END LANGRANGE & OBJ
 
+# TODO: Read through, document and reformat if needed.
     for commodity in commodityList:
         for node in nodeList:
             if node[0] != "s" and (node[0] != "t"):
@@ -93,8 +131,8 @@ def modeler(arcDict, nodeList, commodityList, roomDict, roomCapDict, commodityVo
                 outDict = tupledict(outDict)
                 m.addConstr(inDict.sum() == outDict.sum())
                 #print("\n\n", node, ":  \ninDict: \n", inDict, "\noutDict\n", outDict)
-    varDict = tupledict(varDict)
-    # m.setObjective(varDict.sum(), GRB.MINIMIZE)
+
+    varDict = tupledict(varDict) # TODO: Purpose?? not used again
     m.setObjective(objective, GRB.MINIMIZE)
     m.optimize()
 
@@ -158,7 +196,7 @@ def printSolution(m):
         print('\nObjective Value: %g' % m.objVal)
         for var in m.getVars():
             if var.X > 0.0:
-                # remove dummy arcs
+                # TODO: remove dummy arcs
                 print("{:<55s}| {:>8.0f}".format(var.VarName, var.X))
     else:
         print('No solution;', m.status)
@@ -166,22 +204,18 @@ def printSolution(m):
 
 
 def main(args):
-    arcDict, nodeList, commodityList = arcDataReader("MCNFDataTest.csv")
-    roomDict = roomDictReader("RoomDictionary.csv")
+    arcDict, lagrangeDict, nodeList, commodityList = arcDataReader("MCNFDataTest.csv")
+    roomKey = roomKeyReader("roomDictionary.csv")
     #print(arcDict)
     #print("\n\n")
     #print(nodeList)
     #roomCapDict = {"S1": 100000, "S2": 100000, "S3": 100000, "S4": 100000, "S5": 100000, "S6": 100000, "S7": 100000, "S8": 100000, "S9": 100000, "S10": 100000}
     roomCapDict = {"1": 10000}
     commodityVolDict = {"1": 2, "2": 3}
-    m = modeler(arcDict, nodeList, commodityList, roomDict, roomCapDict, commodityVolDict)
+    m = modeler(arcDict, lagrangeDict, nodeList, commodityList, roomKey, roomCapDict, commodityVolDict)
     printSolution(m)
 
 
 if __name__ == '__main__':
     import sys
     main(sys.argv)
-
-
-
-
